e14e692ddfbabf0159b596dcd449c40efb74cf3d
Ihor Mahats <igor1999691@gmail.com>
2024-11-14 18:53:27 +0200

    add modified Task 3

--- Task 3/hw_3.3/index1.js
+++ Task 3/hw_3.3/index1.js
@@ -1,3 +1,7 @@
 let n = prompt('Enter a five-digit number');
-n = "1 0 3 6 9";
-console.log(n);
+let result = "";
+while (n > 0) {
+    result = (n % 10) + " " + result;
+    n = Math.floor(n / 10);
+}
+console.log(result.trim());
