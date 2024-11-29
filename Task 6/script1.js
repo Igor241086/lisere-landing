13760227bc8e3391269d315b473cff601315fa34
Ihor Mahats <igor1999691@gmail.com>
2024-11-29 16:42:26 +0200

    add modified 6.1, add Task 7 and Task 8

--- Task 6/script1.js
+++ Task 6/script1.js
@@ -1,23 +1,29 @@
 function removeChars(string, charsToRemove) {
-    let result = "";
-    for (let i = 0; i < string.length; i++) {
+  let result = "";
+  for (let i = 0; i < string.length; i++) {
       let include = false;
       for (let j = 0; j < charsToRemove.length; j++) {
-        if (string[i] === charsToRemove[j]) {
-          include = true;
-          break;
-        }
+          if (string[i] === charsToRemove[j]) {
+              include = true;
+              break;
+          }
       }
       if (!include) {
-        result += string[i];
+          result += string[i];
       }
-    }
-    return result;
   }
+  return result;
+}
 
-  let userString = prompt("Enter a string:");
+let userString = prompt("Enter a string:");
+if (userString !== null) {
   let charsToRemoveInput = prompt("Enter the characters to remove:");
-  
-  let charsToRemove = charsToRemoveInput.split('');
-  
-  console.log(removeChars(userString, charsToRemove));
+  if (charsToRemoveInput !== null) {
+      let charsToRemove = charsToRemoveInput.split('');
+      console.log(removeChars(userString, charsToRemove));
+  } else {
+      console.log("Operation canceled: no characters to remove provided.");
+  }
+} else {
+  console.log("Operation canceled: no input string provided.");
+}
