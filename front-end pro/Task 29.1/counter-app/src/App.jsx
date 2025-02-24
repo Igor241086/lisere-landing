b702d1f6ddb3f77c3e15833edd235e8a0cb96eb2
Ihor Mahats <igor1999691@gmail.com>
2025-02-24 15:24:06 +0200

    Added Task 29.1

---
+++ front-end pro/Task 29.1/counter-app/src/App.jsx
@@ -0,0 +1,10 @@
+import { Counter } from "./features/counter/Counter";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  );
+}
