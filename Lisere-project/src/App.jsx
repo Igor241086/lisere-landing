0d47e29a8d1a2591f08f6269a9aa32ebe223ef23
Ihor Mahats <igor1999691@gmail.com>
2025-06-04 18:40:04 +0300

    chore: initial project setup with SCSS and gitignore

---
+++ Lisere-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Hero from './assets/components/Hero.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Hero />
+        
+    </>
+  );
+}
+
+export default App;
