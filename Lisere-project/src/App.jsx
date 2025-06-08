da2d4112e8c66808452f1f32b07070db9c5a2ad7
Ihor Mahats <igor1999691@gmail.com>
2025-06-08 20:01:11 +0300

    Build Hero section: structured grid layout, styled text components, animated CTA button, and footer disclaimer

--- Lisere-project/src/App.jsx
+++ Lisere-project/src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx'
 
-import './styles/base.scss';
-import './styles/grid.scss';
-import './styles/variables.scss';
+import './styles/main.scss';
 
 function App() {
   return (
