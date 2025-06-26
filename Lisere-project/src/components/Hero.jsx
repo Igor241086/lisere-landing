139722f61967f38b9f2177f9b3f4851b42b1de99
Ihor Mahats <igor1999691@gmail.com>
2025-06-26 18:52:38 +0300

    Make Hero and Header responsive; add pseudo background with fade-in; refactor layout and styles to use variables; improve grid system and base setup

--- Lisere-project/src/components/Hero.jsx
+++ Lisere-project/src/components/Hero.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Hero.scss';
-import bgImage from '../assets/images/hero-bg.jpg';
+import bgImage from '../assets/images/hero-bg.png';
 
 const Hero = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bgImage;
+    img.onload = () => setLoaded(true);
+  }, []);
+
   return (
-    <section className="hero">
-      <div className="hero__image-wrapper">
-        <img src={bgImage} alt="hero" className="hero__image" />
-      </div>
-      <div className="grid-15 hero__content">
-        <div className="hero__centered-block">
+    <section className={`hero ${loaded ? 'hero--loaded' : ''}`}>      
+      <div className="grid-15x13 hero__content">
+        <div className="col-start-2 col-span-13 hero__centered-block">
           <div className="hero__text hero__text--main fade-in delay-1">
             WHEN FORM FOLLOWS
           </div>
@@ -24,9 +29,6 @@ const Hero = () => {
               DISCOVER THE CONCEPT
             </button>
           </div>
-        </div>
-        <div className="hero__disclaimer">
-          Photo: Publicly available image (used for conceptual presentation only).
         </div>
       </div>
     </section>
